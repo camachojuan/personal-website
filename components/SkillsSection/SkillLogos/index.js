da9e6b6c6f6655f89e881b2f9929b7792fd77a12
Juan Camacho <camachojuan@gmail.com>
2020-08-19 11:25:44 -0300

    Formatting code

--- components/SkillsSection/SkillLogos/index.js
+++ components/SkillsSection/SkillLogos/index.js
@@ -3,18 +3,17 @@ import styles from "./skillLogos.module.css";
 
 const SkillsSection = () => {
   return (
-      <>
-    <div className={styles.logos}>
-      <SkillLogo image="/images/skill-logos/javascript.svg" name="JavaScript" />
-      <SkillLogo image="/images/skill-logos/react.svg" name="React" />
-      <SkillLogo image="/images/skill-logos/graphql.svg" name="GraphQL" />
-      <SkillLogo image="/images/skill-logos/nodejs.svg" name="Node.js" />
-      <SkillLogo image="/images/skill-logos/aws.svg" name="AWS" />
-      <SkillLogo image="/images/skill-logos/serverless.svg" name="Serverless" />
-   
-    </div>
-       <div className={styles.manyMore}>and many more...</div>
-       </>
+    <>
+      <div className={styles.logos}>
+        <SkillLogo image="/images/skill-logos/javascript.svg" name="JavaScript" />
+        <SkillLogo image="/images/skill-logos/react.svg" name="React" />
+        <SkillLogo image="/images/skill-logos/graphql.svg" name="GraphQL" />
+        <SkillLogo image="/images/skill-logos/nodejs.svg" name="Node.js" />
+        <SkillLogo image="/images/skill-logos/aws.svg" name="AWS" />
+        <SkillLogo image="/images/skill-logos/serverless.svg" name="Serverless" />
+      </div>
+      <div className={styles.manyMore}>and many more...</div>
+    </>
   );
 };
 
